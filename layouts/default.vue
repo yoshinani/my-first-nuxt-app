b49ba582c7aab6fc3bfc17cd30ca365b299e1a2e
yutanakano <yutanakano395@icloud.com>
2019-05-12 05:15:24 +0900

    layoutsディレクトリによるレイアウトの共通化

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,25 @@
 <template>
   <div>
+    <span>default layout</span>
+    <AppNavigation />
+    <hr>
     <nuxt/>
+    <hr>
+    <footer>
+      footer
+    </footer>
   </div>
 </template>
+
+<script>
+import AppNavigation from '~/components/AppNavigation.vue'
+export default {
+  components: {
+    AppNavigation
+  }
+}
+</script>
+
 
 <style>
 html {
--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,7 @@
 <template>
-  <section class="container">
-    <div>
-      <h3>Nuxt.js のタグがつけられた投稿の一覧</h3>
-      <ul>
-        <li v-for="item in items" :key="item.id">
-          <h4>
-            <span>{{item.title}}</span>
-            <small>
-              <span>by </span>
-              <nuxt-link :to="`/users/${item.user.id}`">
-                {{item.user.id}}
-              </nuxt-link>
-            </small>
-          </h4>
-          <div>{{item.body.slice(0, 130)}}</div>
-          <p><a :href="item.url">{{item.url}}</a></p>
-        </li>
-      </ul>
-    </div>
-  </section>
+  <div>
+    <h1>Index page</h1>
+  </div>
 </template>
 
 <script>
