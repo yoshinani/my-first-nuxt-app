4f30c4144fbc5a637b5d394f584a03ef16023deb
yutanakano <yutanakano395@icloud.com>
2019-05-15 00:03:33 +0900

    middlewareによるグローバルなフックの登録

--- pages/authed-route.vue
+++ pages/authed-route.vue
@@ -1,14 +1,10 @@
 <template>
   <div>
-    <h2>Index page</h2>
-    <ul>
-      <li>
-        <nuxt-link to="/login">ログインページへ</nuxt-link>
-      </li>
-      <li>
-        <nuxt-link to="authed-route">認証が必要なページへ</nuxt-link>
-      </li>
-    </ul>
+    <h2>認証が必要なページ</h2>
+    <p>
+      Cookie の 'credential' 値が Truthy な場合のみアクセス可能です。<br>
+      <nuxt-link to="/">トップページへ戻る</nuxt-link>
+    </p>
   </div>
 </template>
 
---
+++ pages/login.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h2>ログインページ</h2>
+    <p>
+      <button type="button" @click="login">ログイン</button><br>
+      <nuxt-link to="/">トップページへ戻る</nuxt-link>
+    </p>
+  </div>
+</template>
+
+<script>
+import Cookies from 'universal-cookie'
+export default {
+  methods: {
+    login() {
+      const cookies = new Cookies()
+      cookies.set('credential', 'true', {maxAge: 90})
+      this.$router.push('/')
+    }
+  }
+};
+</script>
+
+<style>
+.container {
+  min-height: 100vh;
+  padding: 16px;
+}
+
+h3 {
+  margin: 16px 0;
+  padding: 8px 0;
+  border-bottom: solid 1px #e5e5e5;
+}
+
+li + li {
+  margin: 16px 0;
+}
+
+p {
+  margin: 8px 0;
+}
+</style>
+
